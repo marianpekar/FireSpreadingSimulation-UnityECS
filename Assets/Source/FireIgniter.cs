718022d84c550d10834cb08cb6fbcf60520f1f34
Marian Pekár <marian.pekar@saabczech.cz>
2020-08-31 16:31:27 +0200

    Add FireIgniter to randomly ignite several flamables

---
+++ Assets/Source/FireIgniter.cs
@@ -0,0 +1,46 @@
+﻿namespace MarianPekar.FireSpreadingSimulation
+{
+    using Unity.Collections;
+    using Unity.Entities;
+    using UnityEngine;
+
+    public class FireIgniter : MonoBehaviour
+    {
+        [SerializeField] private Spawner spawner;
+
+        [SerializeField] private int ignitionCount;
+        private NativeArray<Entity> entities;
+
+        public void IgniteRandomFlamables()
+        {
+            entities = spawner.Manager.CreateEntityQuery(typeof(FlamableData)).ToEntityArray(Allocator.Persistent);
+
+            if(entities.Length == 0)
+                return;
+
+            if (ignitionCount > entities.Length)
+            {
+                IgniteAll(entities);
+                return;
+            }
+
+            for (var i = 0; i < ignitionCount; i++)
+                spawner.Manager.SetComponentData(entities[Random.Range(0,entities.Length-1)], new FlamableData { State = FlamableState.OnFire });
+
+            entities.Dispose();
+        }
+
+        private void IgniteAll(NativeArray<Entity> entities)
+        {
+            foreach (var entity in entities)
+                spawner.Manager.SetComponentData(entity, new FlamableData { State = FlamableState.OnFire });
+
+            entities.Dispose();
+        }
+
+        public void OnDestroy()
+        {
+            entities.Dispose();
+        }
+    }
+}
